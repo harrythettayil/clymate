2b239ad3dcf11970fb68cc12e826dbf7debbf47b
Harry <t.a.harry@gmail.com>
2025-05-08 14:18:57 +0530

    dependencies installed, made header, env file created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import Dashboard from "./pages/dashboard";
+import City from "./pages/city";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
 
+  const queryClient = new QueryClient()
+  
   return (
-    <div className="text-3xl font-bold underline">whatsup</div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/city/:cityName" element={<City />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
