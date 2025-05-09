27a415ddc6e245997e4762fbf78476480aa598ab
Harry <t.a.harry@gmail.com>
2025-05-09 19:28:15 +0530

    logo path

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import { ThemeToggle } from "./theme-toggle";
 import { CitySearch } from "./city-search";
+import logo from "../../public/clymate.svg"
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to={"/"}>
-          <img src="./clymate.svg" alt="clymate logo" className="h-14" />
+          <img src={logo} alt="clymate logo" className="h-14" />
         </Link>
         <div className="flex gap-4">
           <CitySearch />
