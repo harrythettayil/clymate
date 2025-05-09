6f50ec662b9a15a7fb6e0bab93475d48c9c10949
Harry <t.a.harry@gmail.com>
2025-05-09 19:02:04 +0530

    path fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to={"/"}>
-          <img src="/clymate.svg" alt="clymate logo" className="h-14" />
+          <img src="./clymate.svg" alt="clymate logo" className="h-14" />
         </Link>
         <div className="flex gap-4">
           <CitySearch />
